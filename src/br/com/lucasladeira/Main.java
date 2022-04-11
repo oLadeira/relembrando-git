54f9f764d422d9ea1aff0be6453f77d0e150ea9a
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-11 14:28:27 -0300

    Hello World 4

--- src/br/com/lucasladeira/Main.java
+++ src/br/com/lucasladeira/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World 2!");
-        System.out.println("Hellow World 3");
+        System.out.println("Hello World 3");
+        System.out.println("Hello World 4");
     }
 }
